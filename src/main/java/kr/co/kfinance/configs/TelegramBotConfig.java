184cb2808b51d4b43b47584dca0b3d0e00f23d79
empering <empering@naver.com>
2021-07-08 16:16:47 +0900

    로컬에서 오류나는부분 주석했는데 해제함. 원내 보안정책에 의해 막히는듯

--- src/main/java/kr/co/kfinance/configs/TelegramBotConfig.java
+++ src/main/java/kr/co/kfinance/configs/TelegramBotConfig.java
@@ -23,30 +23,30 @@ public class TelegramBotConfig {
 	public TelegramBot telegramBot() {
 		TelegramBot telegramBot = new TelegramBot(appProperties.getTelegrambotToken());
 
-//		telegramBot.setUpdatesListener(updates -> {
-//			for (Update update : updates) {
-//				if (update.message() == null) {
-//					continue;
-//				}
-//
-//				String text = update.message().text();
-//				Chat chat = update.message().chat();
-//
-//				if (text.equals("/start") || text.equals("/stop")) {
-//					TelegramChat telegramChat = TelegramChat.builder()
-//							.chatId(chat.id())
-//							.userName(chat.username())
-//							.firstName(chat.firstName())
-//							.lastName(chat.lastName())
-//							.start(text.equals("/start"))
-//							.build();
-//
-//					telegramChatRepository.save(telegramChat);
-//				}
-//			}
-//
-//			return UpdatesListener.CONFIRMED_UPDATES_ALL;
-//		});
+		telegramBot.setUpdatesListener(updates -> {
+			for (Update update : updates) {
+				if (update.message() == null) {
+					continue;
+				}
+
+				String text = update.message().text();
+				Chat chat = update.message().chat();
+
+				if (text.equals("/start") || text.equals("/stop")) {
+					TelegramChat telegramChat = TelegramChat.builder()
+							.chatId(chat.id())
+							.userName(chat.username())
+							.firstName(chat.firstName())
+							.lastName(chat.lastName())
+							.start(text.equals("/start"))
+							.build();
+
+					telegramChatRepository.save(telegramChat);
+				}
+			}
+
+			return UpdatesListener.CONFIRMED_UPDATES_ALL;
+		});
 
 		return telegramBot;
 	}
