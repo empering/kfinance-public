9c9d8e704925c9bc2c6ca345288deffcba560d59
empering <empering@naver.com>
2021-07-17 22:22:41 +0900

    로컬 테스트를 위한 MockData 생성 추가

---
+++ src/main/java/kr/co/kfinance/configs/ApplicationMockData.java
@@ -0,0 +1,42 @@
+package kr.co.kfinance.configs;
+
+import kr.co.kfinance.inquiries.Inquiry;
+import kr.co.kfinance.inquiries.InquiryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.stream.IntStream;
+
+@Profile("default")
+@Configuration
+public class ApplicationMockData {
+
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return new ApplicationRunner() {
+
+			@Autowired
+			InquiryRepository inquiryRepository;
+
+			@Override
+			public void run(ApplicationArguments args) {
+				IntStream.range(0, 50).forEach(this::generateInquiry);
+			}
+
+			private void generateInquiry(int index) {
+				Inquiry inquiry = Inquiry.builder()
+						.name("테스트" + index)
+						.type("가입")
+						.card("보유")
+						.hp("000-0000-0000")
+						.build();
+
+				inquiryRepository.save(inquiry);
+			}
+		};
+	}
+}
